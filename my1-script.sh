f0231aa9c98854eb684acd701a406da0cf69c7e6
Samyak Jain <samyak.jn11@gmail.com>
2019-01-24 00:41:09 +0530

    This involves introduction to shell scrippting.

---
+++ my1-script.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+    echo This is learning stage of shell scripting, trying to grab a new adventure !
+#adding comments
+#Hello World various techniques to print
+    echo Hello World
+    echo Hello World
+    echo Hello *  world
+#for transferring message like variable
+    My_message="Hello World via Var"
+    echo $My_message
